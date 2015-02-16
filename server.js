24704a3f1cdc197428ac0660616e01dae13e863b
Sam Greenwood <sam@samgreenwood.me>
2015-02-16 23:44:43 +1030

    Add recent trigger to the bot

--- server.js
+++ server.js
@@ -1,23 +1,47 @@
 var http = require('http');
 var irc = require('irc');
 var net = require('net');
+var sprintf = require('sprintf');
+var filesize = require('filesize');
 
 var client = new irc.Client('localhost', 'Goonbot', {
-  autoConnect: false
+    autoConnect: false
 });
 
-var server = net.createServer(function(socket) {
-    socket.on('data', function(data)
-    {
-        client.say('#allthefiles', data);
+var server = net.createServer(function (socket) {
+    socket.on('data', function (data) {
+        client.say('#goontest', data);
     });
 });
 
-client.connect(5, function() {
-  console.log("Connected!");
-  client.join('#allthefiles', function() {
-    console.log("Joined #allthefiles");
-  });
+client.connect(5, function () {
+    client.join('#goontest', function () {
+    });
+});
+
+client.addListener("message", function (from, to, text, message) {
+    if (text == "!recent") {
+        http.get('http://dcannounce.goonwood.wan/api/recent', function (response) {
+            response.on('data', function (data) {
+                var channel = message.args[0];
+                var announcements = JSON.parse(data);
+
+                announcements.forEach(function (announcement) {
+                    var size = filesize(announcement.size);
+                    var announceString = sprintf("[%s] %s - %s - %s", announcement.site, announcement.filename, size, announcement.magnet);
+
+                    client.say(channel, announceString);
+                });
+
+
+            })
+        });
+    }
+
+    if (text.lastIndexOf("!as") > -1) {
+        var data = text.substr(text.lastIndexOf("!as"), text.length);
+        console.log(data);
+    }
 });
 
 server.listen('54321')
